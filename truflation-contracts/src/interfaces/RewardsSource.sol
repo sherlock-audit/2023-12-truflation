36ab55ce8a5824aded5efa1a98825df5686fb2ad
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2024-01-04 13:15:22 +0100

    Delete scope directories

--- truflation-contracts/src/interfaces/RewardsSource.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.19;
-
-interface RewardsSource {
-    function previewRewards() external view returns (uint256);
-
-    function collectRewards() external returns (uint256);
-}
